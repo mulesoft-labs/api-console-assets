0953fdbca37c35130dce5ac32112b1dcba01bb04
Nicolas Escalante <nlante@gmail.com>
2017-09-18 10:29:53 -0300

    fix: rollback amd reset

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module.exports.polymerPolyfillScript = function(basePath) {
   return `
     <script>
-      // reset amd methods to avoid conflicts with other libs
-      window.define = undefined;
       window.Polymer = {
         dom: 'shadow'
       };
