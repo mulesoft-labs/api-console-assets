3d4c5332fdd6b20d9595d622cbe62c57e2e1590a
Mariano Campo <mariano.campo@mulesoft.com>
2017-12-06 15:46:18 -0300

    feat: removed custom commitAnalyzer and package-lock.json

--- commitAnalyzer.js
+++
@@ -1,8 +0,0 @@
-const commitAnalyzer = require('@semantic-release/commit-analyzer')
-
-module.exports = function (pluginConfig, config, cb) {
-  commitAnalyzer(pluginConfig, config, (err, releaseType) => {
-    // fallback to patch to allow builds with no commits that just updates the published dependencies
-    cb(err, releaseType || 'patch')
-  })
-}
